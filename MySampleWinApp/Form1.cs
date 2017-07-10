2a751f1d7c6f53621475754f2ed71108dd2c9ec9
Vedank Kulshrestha <vedank.kulshrestha6@gmail.com>
2017-07-10 16:26:57 +0530

    Initial Commit
    This is initial commit for testing continuous integration

---
+++ MySampleWinApp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySampleWinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var resp = CalulateLogicTC1(2,5);
+
+            label1.Text = "Done - " + resp;
+        }
+
+        public int CalulateLogicTC1(int a, int b)
+        {
+            return (a + b) * a;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var resp = CalulateLogicTC2(2, 5);
+
+            label1.Text = "Done - " + resp;
+        }
+
+        public int CalulateLogicTC2(int a, int b)
+        {
+            return a + b * a;
+        }
+    }
+}
---
+++ WinAppUnitTest/TestSampleApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using MySampleWinApp;
+
+namespace WinAppUnitTest
+{
+    [TestFixture]
+    public class TestSampleApp
+    {
+        [Test]
+        public void CalulateLogicTC1_Input2And5_14()
+        {
+            Form1 frm = new Form1();
+            var result = frm.CalulateLogicTC1(2, 5);
+
+            Assert.That(result,Is.EqualTo(14));
+        }
+
+        [Test]
+        public void CalulateLogicTC2_Input2And5_12()
+        {
+            Form1 frm = new Form1();
+            var result = frm.CalulateLogicTC2(2, 5);
+            Assert.That(result, Is.EqualTo(12));
+        }
+    }
+}
