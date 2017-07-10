75229bc1280ed63e822142c90d0325ce57498715
Vedank Kulshrestha <vedank.kulshrestha6@gmail.com>
2017-07-10 16:31:16 +0530

    added missing files
    Missing files added

--- README.md
+++
@@ -1,2 +0,0 @@
-# MySampleWinApp
-Its a sample window application using it for testing purpose for continuous integartion
---
+++ WinAppUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinAppUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WinAppUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("988463cd-685d-46d2-a417-8ad079f69e17")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
